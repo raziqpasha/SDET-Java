0623ba5e7ec4d06157541a9bd97770866c5c4d97
raziqpasha <sjraziq@gmail.com>
2025-03-15 00:51:18 +0530

    adding daily task files

--- src/Daily_Java_Task_2025/Task_12Mar.java
+++ src/Daily_Java_Task_2025/Task_12Mar.java
@@ -1,3 +1,5 @@
+package Daily_Java_Task_2025;
+
 public class Task_12Mar {
     public static void main(String[] args) {
         // ³√(x²+y²-|z|)
@@ -10,7 +12,7 @@ public class Task_12Mar {
         int x=10;
         int y=10;
         int z=10;
-       // int result = Math.cbrt(Math.pow(x,2) + Math.pow(y,2) - Math.abs(z));
+        // int result = Math.cbrt(Math.pow(x,2) + Math.pow(y,2) - Math.abs(z));
 
         System.out.println(Math.cbrt(Math.pow(x,2) + Math.pow(y,2) - Math.abs(z)));
     }
