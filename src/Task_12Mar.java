b40c215b9a04df80d3a4372c9657852113c75ebc
raziqpasha <sjraziq@gmail.com>
2025-03-15 00:44:09 +0530

    task file is updating

---
+++ src/Task_12Mar.java
@@ -0,0 +1,17 @@
+public class Task_12Mar {
+    public static void main(String[] args) {
+        // ³√(x²+y²-|z|)
+        //x² → Square of x
+        //y² → Square of y
+        //|z| → Absolute value of z
+        //Add x² + y², then subtract |z|
+        //Take the cube root (³√) of the result
+
+        int x=10;
+        int y=10;
+        int z=10;
+       // int result = Math.cbrt(Math.pow(x,2) + Math.pow(y,2) - Math.abs(z));
+
+        System.out.println(Math.cbrt(Math.pow(x,2) + Math.pow(y,2) - Math.abs(z)));
+    }
+}
