14325b28f91e3d7d67575adc949b18a1c328718d
raziqpasha <sjraziq@gmail.com>
2025-03-23 00:33:42 +0530

    chang task22 to task22mR

--- src/Daily_Java_Task_2025/Task22Mar_fibonacci_Series.java
+++ src/Daily_Java_Task_2025/Task22Mar_fibonacci_Series.java
@@ -1,6 +1,6 @@
 package Daily_Java_Task_2025;
 
-public class Task22_fibonacci_Series {
+public class Task22Mar_fibonacci_Series {
     public static void main(String[] args) {
    int n=0;
    for(int i=0;i<=8;i++){
