bdd1f4e7c3a0240ce2610c5f7d77095b7b548f91
raziqpasha <sjraziq@gmail.com>
2025-03-30 15:55:47 +0530

    task files

---
+++ src/Daily_Java_Task_2025/Task_21Mar_Interview_question.java
@@ -0,0 +1,55 @@
+package Daily_Java_Task_2025;
+
+public class Task_21Mar_Interview_question {
+    public static void main(String[] args) {
+        /*
+        1) If there is no main method, what will happen?
+ans->1) If there is no main method, JVM will not be able to run the program.
+
+2) Does main mehtod return any value?
+ans->2) No, as the return type is void
+
+
+3) what is main method?
+ans->3) Main method is entry point to start the execution.
+
+
+4) (String[] args) what is this in main method?
+ans->4) Arguments are passed through this.
+
+
+5) main(String[] args) in this insted of args can i write something
+ else like java, or Selenium like this (String[] Selenium)?
+ans->5) no, it doesn't allow
+
+
+6) public static void main(string args[]) { } is this right main method,
+is this will work? ([] position is changed)
+ans->6) no - String should be used instead of string
+
+
+7) public static void main(string[]args) { }, is this right main method,
+is this will work? (i have removed the space between []args)
+ans->7) no, space should be added to differentiate
+
+
+8) public static void main(string...args) { }, is this right main method,
+ is this will work? ( i have removed [] and added ...)
+ans->no
+
+9) static public void main(String[] args) { is this valid main mehtod ?
+ans->no
+
+10) public void static main(String[] args) {} is this valid main method?
+ans->no
+
+11) public static main void (String[] args) {} is this valid main method?
+ans->no
+
+12) Who Gets Highest Priority in Execution?
+ans->12) Main method
+
+
+         */
+    }
+}
